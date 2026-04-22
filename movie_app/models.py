e98f5982b8d4af92f5e3658828624ff56d90e528
Aysha <23201086@uap-bd.edu>
2026-04-22 09:45:51 +0600

    movie app models and admin complete

---
+++ movie_app/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    release_date = models.DateField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title

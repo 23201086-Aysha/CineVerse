c802886d428c69bfabfdb542d5e3b0e59e6dfe00
Samsun Nahar Niha <23201100@uap-bd.edu>
2026-04-27 11:36:15 +0600

    class added ,views.py fixed

--- cineverses/settings.py
+++ cineverses/settings.py
@@ -37,6 +37,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'user',
     'movie_app',
+    'review',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- review/models.py
+++ review/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from movie_app.models import Movie
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField()
+    is_spoiler = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class ReviewReply(models.Model):
+    review = models.ForeignKey(Review, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    reply_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)

511ce859981bc7017f622c349f17422577de59c2
Samsun Nahar Niha <23201100@uap-bd.edu>
2026-05-04 11:20:19 +0600

    View.py and urls.py added in review app

--- review/urls.py
+++ review/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-
 from . import views
 
-app_name = "review"
-
 urlpatterns = [
+    path('movie/<int:movie_id>/', views.movie_reviews, name='movie_reviews'),
+    path('add/<int:movie_id>/', views.add_review, name='add_review'),
+    path('reply/<int:review_id>/', views.add_reply, name='add_reply'),
+    path('delete/<int:review_id>/', views.delete_review, name='delete_review'),
 ]
--- review/views.py
+++ review/views.py
@@ -1,3 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Review, ReviewReply
+from movie_app.models import Movie
 
-# Create your views here.
+
+def movie_reviews(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    reviews = Review.objects.filter(movie=movie)
+
+    return render(request, 'reviews/movie_reviews.html', {
+        'movie': movie,
+        'reviews': reviews
+    })
+
+
+# review add
+@login_required
+def add_review(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+
+    if request.method == 'POST':
+        rating = request.POST.get('rating')
+        comment = request.POST.get('comment')
+        is_spoiler = request.POST.get('is_spoiler') == 'on'
+
+        Review.objects.create(
+            user=request.user,
+            movie=movie,
+            rating=rating,
+            comment=comment,
+            is_spoiler=is_spoiler
+        )
+
+    return redirect('movie_reviews', movie_id=movie.id)
+
+
+# reply add
+@login_required
+def add_reply(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    if request.method == 'POST':
+        reply_text = request.POST.get('reply_text')
+
+        ReviewReply.objects.create(
+            review=review,
+            user=request.user,
+            reply_text=reply_text
+        )
+
+    return redirect('movie_reviews', movie_id=review.movie.id)
+
+
+# review delete
+@login_required
+def delete_review(request, review_id):
+    review = get_object_or_404(Review, id=review_id)
+
+    if review.user == request.user:
+        review.delete()
+
+    return redirect('movie_reviews', movie_id=review.movie.id)
